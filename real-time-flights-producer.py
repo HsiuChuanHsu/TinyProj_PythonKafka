a8ea49cf3978de7b2dd3ffc573834e952b2e083b
HsiuChuanHsu <hchsu2106@gmail.com>
2023-10-15 12:00:59 +0800

    update producer

--- real-time-flights-producer.py
+++ real-time-flights-producer.py
@@ -4,35 +4,41 @@ import requests
 
 from kafka import KafkaProducer
 
-FLIGHT_KAFKA_TOPIC = "Flight-Info"
+def create_kafka_producer():
+    producer = KafkaProducer(
+        bootstrap_servers="localhost:29092")
+    return producer
 
-producer = KafkaProducer(
-    bootstrap_servers="localhost:29092")
+def get_Airlabs_data():
+    # Calling AirLab API
+    API_KEY = "dc019782-c207-4c85-85d3-bb4d4e1d0845"
+    url = f"https://airlabs.co/api/v9/flights?api_key={API_KEY}"
+
+    payload = {}
+    headers = {}
+
+    response = requests.request(
+        "GET"
+        , url
+        , headers=headers
+        , data=payload)
+
+    data = response.json()    
+    return data
 
 
-# Calling AirLab API
-API_KEY = "dc019782-c207-4c85-85d3-bb4d4e1d0845"
-url = f"https://airlabs.co/api/v9/flights?api_key={API_KEY}"
-
-payload = {}
-headers = {}
-
-response = requests.request(
-    "GET"
-    , url
-    , headers=headers
-    , data=payload)
-
-data = response.json()
-datalist = data['response']
 
 # Loop Over data and send to topic
-for i in range(0, len(data['response'])):
+FLIGHT_KAFKA_TOPIC = "Flight-Info"
+producer = create_kafka_producer()
+data = get_Airlabs_data()
+
+for idx, x in enumerate(data['response']):
     
     producer.send(
         FLIGHT_KAFKA_TOPIC
-        , json.dumps(datalist[i]).encode("utf-8")
+        , json.dumps(x).encode("utf-8")
     )
     
-    print(f"Done Sending..{i}")
+    print(f"Done Sending..{idx}")
     time.sleep(0.2)
